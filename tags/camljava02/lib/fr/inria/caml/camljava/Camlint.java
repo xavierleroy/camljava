9d09f74ea9b1b5038cbd804d9c2142e4c55eb097
nobody <nobody@577357e9-00f2-488a-a1d9-421327f133ed>
2003-10-03 11:12:56 +0000

    This commit was manufactured by cvs2svn to create tag 'camljava02'.

---
+++ tags/camljava02/lib/fr/inria/caml/camljava/Camlint.java
@@ -0,0 +1,6 @@
+package fr.inria.caml.camljava;
+
+public class Camlint {
+    public int contents;
+    public Camlint(int c) { contents = c; }
+}
---
+++ tags/camljava02/lib/fr/inria/caml/camljava/Long.java
@@ -0,0 +1,6 @@
+package fr.inria.caml.camljava;
+
+public class Long {
+    public long contents;
+    public Long(long c) { contents = c; }
+}
